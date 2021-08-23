a3d5d5cb57e70252fba9c7b11bdf758fa9bc4dee
AhmadSupriyatna <ahmadsupriyatna1@gmail.com>
2021-08-23 11:58:11 +0700

    Tugas 17 Javascript
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas17-1.js
@@ -0,0 +1,10 @@
+function tugasTujuhBelas(){
+    var a = "Saya beLajar bahaSa peMrograman deNgan khUsuk";
+    var b = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual";
+
+    console.log("Lowercase : ", a.toLowerCase());
+    console.log("Upercase : ", a.toUpperCase());
+    console.log("Lowercase : ", b.toLowerCase());
+    console.log("Upercase : ", b.toUpperCase());
+}
+tugasTujuhBelas()
